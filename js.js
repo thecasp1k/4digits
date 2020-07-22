e2f7945e01b0dfd804fc188c1ebb413ea656133e
TheCasp1k <casp1k96@gmail.com>
2020-07-22 16:58:29 +0300

    Added logic game

--- js.js
+++ js.js
@@ -1,9 +1,10 @@
+let clog = console.log;
+
 let numb;
-(
-    function () {
-        return numb = Math.floor(1000 + Math.random() * (9999+1-1000));
-    }
-);
+function numberRandom () {
+    return numb = Math.floor(1000 + Math.random() * (10000-1000)) + '';
+}
+window.onload = numberRandom();
 
 function gebi (id) {  return document.getElementById(id)};
 function gebcl (className) { return document.getElementsByClassName(className)[0]};
@@ -18,17 +19,25 @@ gebi('btn').onclick = () => {
 
 function gameFunction (number) {
     gebcl('attempt').innerHTML++;
-    console.log(numb);
+    let numbF = numb.split('');
+    let numberF = number.split('');
+    let numberHide = ["*","*","*","*"];
+    console.log(numbF);
+    clog(numberF)
 
 
-
-
-
-
-
-
-
-
+    for( let key in numberF ) {
+        for ( let key1 in numbF ) {
+            if(numberF[key] == numbF[key1] && key == key1) {
+                numberHide[key] = 'B';
+                gebcl('number').innerText[key] = numberF[key];
+                continue;
+            } else if (numberF[key] == numbF[key1] && key != key1) {
+                numberHide[key] = 'C';
+                continue;
+            }
+        }
+    }
 
     gebcl('tableGame').children[0].appendChild(document.createElement('tr')).appendChild(document.createElement('td')).innerText = number;
 }
